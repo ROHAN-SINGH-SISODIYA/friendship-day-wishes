95227dd13596eb7d88298d973cc1bddf80eeb145
Rohan Singh Sisodiya <41806939+ROHAN-SINGH-SISODIYA@users.noreply.github.com>
2019-08-03 22:55:36 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Friendship-day-wishes</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <style type="text/css">
+  	/*	Reset & General
+---------------------------------------------------------------------- */
+* { margin: 0px; padding: 0px; }
+body {
+	background: #ecf1f5;
+	font:14px "Open Sans", sans-serif; 
+	text-align:center;
+}
+
+.tile{
+	width: 100%;
+	background:#fff;
+	border-radius:5px;
+	box-shadow:0px 2px 3px -1px rgba(151, 171, 187, 0.7);
+	float:left;
+  	transform-style: preserve-3d;
+  	margin: 10px 5px;
+
+}
+
+.header{
+	border-bottom:1px solid #ebeff2;
+	padding:19px 0;
+	text-align:center;
+	color:#59687f;
+	font-size:600;
+	font-size:19px;	
+	position:relative;
+}
+
+.banner-img {
+	padding: 5px 5px 0;
+}
+
+.banner-img img {
+	width: 100%;
+	border-radius: 5px;
+}
+
+.dates{
+	border:1px solid #ebeff2;
+	border-radius:5px;
+	padding:20px 0px;
+	margin:10px 20px;
+	font-size:16px;
+	color:#5aadef;
+	font-weight:600;	
+	overflow:auto;
+}
+.dates div{
+	float:left;
+	width:50%;
+	text-align:center;
+	position:relative;
+}
+.dates strong,
+.stats strong{
+	display:block;
+	color:#adb8c2;
+	font-size:11px;
+	font-weight:700;
+}
+.dates span{
+	width:1px;
+	height:40px;
+	position:absolute;
+	right:0;
+	top:0;	
+	background:#ebeff2;
+}
+.stats{
+	border-top:1px solid #ebeff2;
+	background:#f7f8fa;
+	overflow:auto;
+	padding:15px 0;
+	font-size:16px;
+	color:#59687f;
+	font-weight:600;
+	border-radius: 0 0 5px 5px;
+}
+.stats div{
+	border-right:1px solid #ebeff2;
+	width: 33.33333%;
+	float:left;
+	text-align:center
+}
+
+.stats div:nth-of-type(3){border:none;}
+
+div.footer {
+	text-align: right;
+	position: relative;
+	margin: 20px 5px;
+}
+
+div.footer a.Cbtn{
+	padding: 10px 25px;
+	background-color: #DADADA;
+	color: #666;
+	margin: 10px 2px;
+	text-transform: uppercase;
+	font-weight: bold;
+	text-decoration: none;
+	border-radius: 3px;
+}
+
+div.footer a.Cbtn-primary{
+	background-color: #5AADF2;
+	color: #FFF;
+}
+
+div.footer a.Cbtn-primary:hover{
+	background-color: #7dbef5;
+}
+
+div.footer a.Cbtn-danger{
+	background-color: #fc5a5a;
+	color: #FFF;
+}
+
+div.footer a.Cbtn-danger:hover{
+	background-color: #fd7676;
+}
+  </style>
+</head>
+<body>
+<div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <div class="tile">
+                    <div class="wrapper">
+                        <div class="header">Friendship Day</div>
+
+                        <div class="banner-img">
+                            <img src="download.jpeg" alt="Image 1">
+                        </div>
+
+
+
+                        <div class="stats">
+
+                            <div>
+                                <strong>INVITED</strong> 3098
+                            </div>
+
+                            <div>
+                                <strong>JOINED</strong> 562
+                            </div>
+
+                            <div>
+                                <strong>DECLINED</strong> 182
+                            </div>
+
+                        </div>
+
+                        <div class="stats">
+
+                            <div>
+                                <strong>INVITED</strong> 3098
+                            </div>
+
+                            <div>
+                                <strong>JOINED</strong> 562
+                            </div>
+
+                            <div>
+                                <strong>DECLINED</strong> 182
+                            </div>
+
+                        </div>
+
+                        <div class="stats">
+
+                            <div>
+                                <strong>INVITED</strong> 3098
+                            </div>
+
+                            <div>
+                                <strong>JOINED</strong> 562
+                            </div>
+
+                            <div>
+                                <strong>DECLINED</strong> 182
+                            </div>
+
+                        </div>
+
+                        <div class="footer">
+                            <a href="#" class="btn btn-success">Group Share</a>
+                            <a href="#" class="Cbtn Cbtn-danger">personal Share</a>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </div>
+</body>
+</html>
